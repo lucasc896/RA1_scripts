952b01b68306c0dc0be932aadd545af0f0a417f1
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-02-28 16:18:18 +0000

    move data container to another file

--- alphat_excess.py
+++ alphat_excess.py
@@ -5,6 +5,7 @@ import os
 from itertools import product
 from sys import exit
 from simplify_tables import harvest_values, remove_whitespace
+from object_cat import event_cat
 
 # options = argparse.ArgumentParser(description ='Produce RA1 Results')
 
@@ -20,63 +21,6 @@ from simplify_tables import harvest_values, remove_whitespace
 
 r.gStyle.SetOptStat(0)
 grabr.set_palette()
-
-class event_cat(object):
-    """data container for event yields"""
-    def __init__(self, data = [], pred = [], pred_err = []):
-        self._data = data
-        self._pred = pred
-        self._pred_err = pred_err
-        self.check_list_consistency()
-        self._nbins = len(self._data)
-        self.check_val_types()
-
-        self._excess = []
-        self._excess_err = []
-        self.calculate_excess()
-
-    def __str__(self):
-        out_str = ""
-        out_str += ">> event_cat object:\n"
-        out_str += "\t> Data:\n"
-        # out_str += "\t\t" + ", ".join(self._data) + "\n"
-        out_str += "\t\t"
-        for n in range(self._nbins):
-            out_str += "%s, " % self._data[n]
-        out_str += "\n\t> Preds:\n"
-        out_str += "\t\t"
-        for n in range(self._nbins):
-            out_str += "%s+/-%s, " % (self._pred[n], self._pred_err[n])
-        print type(self._data[0])
-
-    def check_list_consistency(self):
-        if len(self._data) != len(self._pred):
-            print "data and preds arrays different lengths"
-            print self._data
-            print self._pred
-        if len(self._pred) != len(self._pred_err):
-            print "preds and pred_errs arrays different lengths"
-            print self._pred
-            print self._pred_err
-
-    def check_val_types(self):
-        """check all val types are consistently floats"""
-        
-        def convert_val(val = ''):
-            if val == "-":
-                return 0.
-            return float(val)
-
-        for n in range(self._nbins):
-            self._data[n] = convert_val(self._data[n])
-            self._pred[n] = convert_val(self._pred[n])
-            self._pred_err[n] = convert_val(self._pred_err[n])
-
-    def calculate_excess(self):
-        for d, p, perr in zip(self._data, self._pred, self._pred_err):
-            self._excesss.append(d-p)
-            self._excesss_err.append(perr)
-
 
 def dict_printer(dicto = {}, indent = 1):
 
