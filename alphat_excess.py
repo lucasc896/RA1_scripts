3e8df0cbc7b9eae104493d1f3c0a330ef87e7c2c
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-03-02 11:28:52 +0000

    towards comparing qcd and excess; get qcd yields and return as an event_cat object

--- alphat_excess.py
+++ alphat_excess.py
@@ -70,7 +70,9 @@ def get_file_key(str = ''):
     tmp_split = tmp_split.split("_")
     
     bcat = "ge0" if tmp_split[-3] == "Inc" else tmp_split[-3]
-    jcat = "ge2" if tmp_split[-1] == "Inc" else tmp_split[-1]
+    jcat = "ge2" if tmp_split[-1] == "inclusive" else tmp_split[-1]
+
+    jcat = jcat.replace("greq", "ge")
 
     return "%sb_%sj" % (bcat, jcat)
 
@@ -96,12 +98,14 @@ def get_excess():
 
 def get_qcd():
 
-    HTbins = ["200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"][3:]
-    ht_scheme = ["incl", "excl"][0]
+    print ">>> Getting QCD yields."
+
+    HTbins = ["200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"]
+    ht_scheme = ["incl", "excl"][1]
     n_j = ["eq2j", "eq3j", "eq4j", "ge5j"]
-    n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b", "ge1b"][-2:-1]
+    n_b = ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b"]
     selec = ["OneMuon", "DiMuon", "Had"][2]
-    hist_title = ["genMetJetPtFrac", "genMetHTFrac", "neutVectPt_ov_genMet", "numGenNeutrino", "genMetOverJetPt_vs_CSV"][-1:]
+    hist_title = ["AlphaT"][0]
     my_path = "Root_Files_22Feb_gt0p3_latest_aT_0p507_v0"
 
     if ht_scheme == "incl":
@@ -111,43 +115,49 @@ def get_qcd():
         my_iter = HTbins
 
     canv = r.TCanvas()
+        
+    yields = {}
 
-    for my_title in hist_title:
-        is_th2 = False
+    for nb, nj in product(n_b, n_j):
+        vals = []
+        errs = []
         for htbin in my_iter:
-            if ht_scheme == "excl":
-                ht_string = htbin
-            n_bin = 1
-            
-            for nb, nj in product(n_b, n_j):
-                h_mc = grabr.grab_plots(f_path = "/Users/chrislucas/SUSY/AnalysisCode/rootfiles/%s/Had_QCD.root" % my_path,
-                                        sele = selec, h_title = my_title, njet = nj, btag = nb, ht_bins = htbin)
-                if "TH1" in str(type(h_mc)):
-                    h_mc.Draw()
-                elif "TH2" in str(type(h_mc)):
-                    is_th2 = True
-                    h_mc.Draw("colz")
+            h_mc = grabr.grab_plots(f_path = "/Users/chrislucas/SUSY/AnalysisCode/rootfiles/%s/Had_QCD.root" % my_path,
+                                    sele = selec, h_title = hist_title, njet = nj, btag = nb, ht_bins = htbin, quiet = True)
+            this_err = r.Double(0.)
+            # check this is giving the correct, weighted vals for QCD MC
+            val = h_mc.IntegralAndError(1, h_mc.GetNbinsX(), this_err)
+            vals.append(val)
+            errs.append(this_err)
+        # print nb, nj
+        # print yields
+        # print errs
+        this_key = "%s_%s" % (nb, nj)
+        this_cat = event_cat(pred = vals, pred_err = errs)
+        this_cat._catstring = this_key
+        yields[this_key] = this_cat
 
-                if my_title == "neutVectPt_ov_genMet":
-                    h_mc.GetXaxis().SetRangeUser(0., 1.5)
-                    canv.SetLogy(1)
+    # dict_printer(yields)
+    return yields
 
-                if is_th2:
-                    h_mc.GetXaxis().SetTitle("genMet/JetPt")
-                    h_mc.GetYaxis().SetTitle("CSV Disciminator")
-                    h_mc.RebinX(10)
-                    h_mc.RebinY(10)
+def make_plots(excess = {}, qcd = {}):
 
-                canv.Print("out/qcdplots_%s_%s_%s_%s.pdf" % (selec, my_title, nb, nj))
+    for cat in excess:
+        if cat not in qcd.keys():
+            continue
+        excess_val, excess_err = excess[cat].the_excess()
+        qcd_val, qcd_err = excess[cat].the_preds()
+        for i in range(len(excess_val)):
+            print excess_val[i], qcd_val[i]
 
 def main():
     # get excess yields
     excess = get_excess()
+    # get qcd yields
+    qcd = get_qcd()
 
-    for cat in excess:
-        cat_excess, cat_excess_err = excess[cat].get_excess()
-
-    make_plot()
+    # make comparison plots
+    make_plots(excess, qcd)
 
 if __name__ == "__main__":
     main()
