db71f50c1cc1482c9805d82e0e546765249c7ec5
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-07-28 22:23:06 +0100

    add table printing; individual comparison plots; commented single page multiplot that doesnt fucking work cos it's stupid and it's not my fault and whatever its late anyway shutup

--- qcd_output.py
+++ qcd_output.py
@@ -55,7 +55,6 @@ class GenericPlotter(object):
     def __init__(self, yields, label = "", allHT = False):
         self._yields = yields._dict
         self._outpath = os.path.join(os.getcwd(), "out", label)
-        print self._outpath
         mkDir(self._outpath)
         self._cpd = self._outpath # 'current plotting directory'
         self._allHT = False # optional flag to only plot inclusive HT
@@ -82,8 +81,8 @@ class GenericPlotter(object):
         # can pick and choose which generic plots to make here
         
         self.makeDPhiComparison()
-        self.makeDPhiComparison(plotRatio = True)
-        self.makeAlphaTDistro(mode = "diff")
+        # self.makeDPhiComparison(plotRatio = True)
+        # self.makeAlphaTDistro(mode = "diff")
         # self.makeAlphaTDistro(mode = "cumu")
 
     def makeAlphaTDistro(self, mode = "diff"):
@@ -106,27 +105,65 @@ class GenericPlotter(object):
 
         self._logy = True
 
+        # megacanv = r.TCanvas("mega", "mega", 1600, 1200)
+        # megacanv.Divide(4, 3, 0.01, 0.01)
+        # should set all the pads here too
+        # index = 1
         for nj in self._bins['nj']:
             for nb in self._bins['nb']:
+                # megacanv.cd(index)
+                # if index > 3: continue
                 for ht in self._bins['ht']:
                     grlt = alphatGraph(lt[nj][nb][ht])
+                    grlt.SetName("#Delta#phi_{min}* < 0.3")
                     grgt = alphatGraph(gt[nj][nb][ht])
+                    grgt.SetName("#Delta#phi_{min}* > 0.3")
                     if plotRatio:
                         grrt = alphatGraph(pytils.dict_div(gt[nj][nb][ht], lt[nj][nb][ht]))
                         self._logy = False
                         self.singleGraph(grrt, self.binString(nj, nb, ht)+"_ratio")
                     else:
                         self.comparisonGraph(grlt, grgt, self.binString(nj, nb, ht))
+                        # lg = r.TLegend(0.74, 0.80, 0.89, 0.89)
+
+                        # grlt.Draw("AP")
+                        # grlt.SetLineColor(r.kRed)
+                        # grlt.SetMarkerColor(r.kRed)
+                        # grlt.SetTitle(self.binString(nj, nb, ht))
+                        # grlt.SetMinimum(0.001)
+                        # grlt.SetMaximum(100000)
+                        # grlt.GetXaxis().SetRangeUser(0.5, 0.6)
+                        # lg.AddEntry(grlt, grlt.GetName(), "L")
+
+                        # grgt.Draw("PSAME")
+                        # grgt.SetLineColor(r.kBlue)
+                        # grgt.SetMarkerColor(r.kBlue)
+                        # lg.AddEntry(grgt, grgt.GetName(), "L")
+
+                        # r.gPad.SetLogy(self._logy)
+
+                        # lg.SetLineColor(0)
+                        # lg.SetFillColor(0)
+                        # lg.Draw()
+                    
+                # index += 1
+        # megacanv.cd()
+        # megacanv.Print(self._cpd + "/" + "all.pdf")
 
         self._logy = False # flick switch back to off
 
     def comparisonGraph(self, gr1 = None, gr2 = None, label = ""):
         canv = r.TCanvas()
+
+        lg = r.TLegend(0.74, 0.80, 0.89, 0.89)
+
         gr1.Draw("AP")
         gr1.SetLineColor(r.kRed)
         gr1.SetMarkerColor(r.kRed)
-
         gr1.SetTitle(label)
+        gr1.SetMinimum(0.001)
+        gr1.SetMaximum(100000)
+        gr1.GetXaxis().SetRangeUser(0.5, 0.6)
 
         gr2.Draw("PSAME")
         gr2.SetLineColor(r.kBlue)
@@ -134,7 +171,15 @@ class GenericPlotter(object):
 
         canv.SetLogy(self._logy)
 
+        for ent in [gr1, gr2]:
+            lg.AddEntry(ent, ent.GetName(), "L")
+        lg.SetLineColor(0)
+        lg.SetFillColor(0)
+        lg.Draw()
+
         canv.Print(self._cpd+"/"+label+".pdf")
+
+        return canv
 
     def singleGraph(self, gr, label):
         canv = r.TCanvas()
@@ -147,6 +192,16 @@ class GenericPlotter(object):
         canv.SetLogy(self._logy)
 
         canv.Print(self._cpd+"/"+label+".pdf")
+
+#---------------------------------------------------------------------#
+
+def printTable(d = {}):
+    # vertical printing
+    for key in sorted(d.keys()):
+        if key == 'inc': continue
+        print repr(round(float(key),3)).ljust(10),
+        print repr(d[key].v).rjust(8), repr(round(d[key].e, 3)).rjust(8)
+
 
 #---------------------------------------------------------------------#
 
