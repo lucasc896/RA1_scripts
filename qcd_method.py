62f1447ba6270f208000dfcfb0507de01b186296
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-07-28 22:20:45 +0100

    add plotting for both ewk subtraction methods

--- qcd_method.py
+++ qcd_method.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pytils
 import ROOT as r
-import qcd_plotting as qplot
+import qcd_output as qout
 from qcd_tools import AnalysisYields as AnaY
 from qcd_tools import Yield, dict_sub_thresh
 from copy import deepcopy
@@ -13,18 +13,21 @@ r.gROOT.SetBatch(1)
 TO-DO
 2. Work out way to present results
     - table
-    - plot
 3. validate threshold subtraction validity
 """
 #---------------------------------------------------------------------#
 
 def bins(key = ""):
     """define the binning to be used"""
-    d = {   "nj":   ["le3j", "ge4j", "ge2j"][-1:],
-            "nb":   ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b"][-1:],
+    d = {   "nj":   ["le3j", "ge4j", "ge2j"],
+            # "nb":   ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b"],
+            "nb":   ["eq0b", "eq1b", "eq2b", "ge0b"],
             "dphi": ["lt0p3", "gt0p3"],
-            "ht":   ["200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"][:4],
-            "at":   [i*0.025 for i in range(40)]}# + [0.5+0.01*i for i in range(20)] + [0.7 + 0.1*i for i in range(4)]}
+            "ht":   ["200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"],
+            # "at":   [i*0.025 for i in range(40)]}# + [0.5+0.01*i for i in range(20)] + [0.7 + 0.1*i for i in range(4)]}
+            # "at":   [0.5 + i*0.001 for i in range(100)],
+            "at":   [0.5 + i*0.002 for i in range(15)] + [0.53+i*0.005 for i in range(14)],
+            }
     
     # convert alphaT float values into strings
     d['at'] = ["%.3f" % at for at in d['at']]
@@ -41,8 +44,7 @@ def bins(key = ""):
 #---------------------------------------------------------------------#
 
 def fpath():
-    # return "/Users/chrislucas/SUSY/AnalysisCode/rootfiles/QCDKiller_GOLDEN/QCDFiles/25March_withBeamHalo_newCC_v0/"
-    return "/Users/chrislucas/SUSY/AnalysisCode/rootfiles/QCDKiller_GOLDEN/QCDFiles/25Jul_fullParked_noDPhi_MHTMETCut_v0/"
+    return "/Users/chrislucas/SUSY/AnalysisCode/rootfiles/QCDKiller_GOLDEN/QCDFiles/28Jul_fullParked_noDPhi_MHTMETCut_v0/"
 
 #---------------------------------------------------------------------#
 
@@ -94,6 +96,7 @@ def makePrediction(hmc = None, md = None, mmc = None):
 #---------------------------------------------------------------------#
 
 def ewkSubtraction(hd = None, hpred = None, thresh = 0.):
+    """dedicated function for Data - EWKPred"""
     newObj = deepcopy(hd)
     newDict = dict_sub_thresh(hd._dict, hpred._dict, thresh)
     newObj._dict['edballs'] = 1.
@@ -109,31 +112,14 @@ def main():
     had_ewk_pred, had_ewk_pred_splitMu = makePrediction(had_mc, mu_data, mu_mc)
 
     had_qcd_pred = ewkSubtraction(had_data, had_ewk_pred)
+    had_qcd_pred_splitMu = ewkSubtraction(had_data, had_ewk_pred_splitMu)
 
-    qplot.GenericPlotter(had_data, "had_data")
-    qplot.GenericPlotter(had_qcd_pred, "had_qcd_pred")
+    # qout.GenericPlotter(had_data, "had_data")
+    qout.GenericPlotter(had_qcd_pred, "had_qcd_pred")
+    qout.GenericPlotter(had_qcd_pred_splitMu, "had_qcd_pred_splitMu")
 
-
-    # grlt0p3 = qplot.alphatGraph(had_mc._dict['lt0p3']['ge2j']['ge0b']['inc'])
-    # grgt0p3 = qplot.alphatGraph(had_mc._dict['lt0p3']['ge2j']['ge0b']['inc'])
-
-
-
-    # canv = r.TCanvas()
-
-    # grgt0p3.Draw("AP")
-    # grgt0p3.SetLineColor(r.kRed)
-    # grgt0p3.SetMarkerColor(r.kRed)
-
-    # grlt0p3.Draw("PSAME")
-    # grlt0p3.SetLineColor(r.kBlue)
-    # grlt0p3.SetMarkerColor(r.kBlue)
-
-    # canv.SetLogy(1)
-
-    # canv.Print("out.pdf")
+    # qout.printTable(had_data._dict['lt0p3']['ge2j']['ge0b']['inc'])
 
 if __name__ == "__main__":
     main()
-    # for at in bins("at"):
-        # print at
+    
